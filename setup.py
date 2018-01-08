567b20496f6a47774e11b9cf66061dc0fecd68ff
Vahid Abdi <vahidabdinet@gmail.com>
2018-01-08 09:52:46 +0330

    Boilerplate setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sensifai_python",
+    version="0.0.1",
+    description="Sensifai API Python Client",
+    packages=find_packages(exclude=["docs", "tests"])
+)

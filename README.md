2fcf726e5ba913744a3f280ad5b7319207dfb507
rahman <r.y.zadeh@sensifai.com>
2018-01-14 14:22:43 +0330

    change structure , modify setup

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
-# Sensifai API Python Client
+Sensifai API Python Client
+====================
 
-## Installation
+Sensifai API Python Client
 
+Overview
+---------------------
+This Python client provides a wrapper around Sensifai <a href="https://developers.sensifai.com"> Image and Video recognition API</a>.
+
+
+Installation
+---------------------
+The API client is available on Pip. You can simply install it with a `pip install`
 ```sh
-pip install sensifai_python
+pip install sensifai 
 ```
+
+For more details on the installation, please refer to https://developers.sensifai.com/installation
+
+Requirements
+---------------------
+You need a SENSIFAI_API_TOKEN.You can get a free limited `token` from https://developers.sensifai.com
 
 ## Usage
 
@@ -17,4 +32,5 @@ media_id = s.video_by_file('your-video-file.mp4')
 
 result = s.predict_video(media_id, ['tagging', 'nsfw', 'action'])
 ```
+
 
--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sensifai_python",
+    name="sensifai",
     version="0.0.1",
+    license='apache-2.0',
     description="Sensifai API Python Client",
     packages=find_packages(exclude=["docs", "tests"]),
-    extras_require=dict(reST="Docutils>=0.14")
+    extras_require=dict(reST="Docutils>=0.14"),
+    
+    maintainer='Rahman yousefzadeh',
+    maintainer_email='r.y.zadeh@Sensifai.com',
+    url='https://github.com/sensifai/sensifai_python',
+    author_email='api@sensifai.com',
+
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Multimedia :: Video'
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3+',
+    ],
+    keywords='Sensofai; Video Recognition; ',
+    python_requires='>=3',
+    
 )

9b8568578506be5fdc453b5da5e41b345e549d0a
sensifai <sensifai@gmail.com>
2019-05-04 13:08:36 +0430

    change return type from tuple to dict

--- sensifai/rest.py
+++ sensifai/rest.py
@@ -61,7 +61,7 @@ class SensifaiApi(object):
             raise ApiError("urls should be list")
 
         payload = {
-        'query':'mutation( $token: String!, $urls: [String!]! ){uploadByUrl( token: $token, urls: $urls){result error taskId cannotUpload}}',
+        'query':'mutation( $token: String!, $urls: [String!]! ){uploadByUrl( token: $token, urls: $urls){result error taskIdList{file taskId} cannotUpload}}',
         'variables':{'urls':urls,
                      'token':self.token}
         }
@@ -79,7 +79,7 @@ class SensifaiApi(object):
                 data = json.loads(conn.text)['data']['uploadByUrl']
                 if data['result']:
                     logger.debug("File uploaded successfully.")
-                    return data['taskId'], data['cannotUpload']
+                    return {'taskIds':data['taskIdList'], 'cannotUpload':data['cannotUpload']}
                 else:
                     logger.error("error: {}").format(data['error'])
             else:
@@ -96,7 +96,7 @@ class SensifaiApi(object):
         file_map = json.dumps({ str(k): ["variables.files.{}".format(k)] for k in range(0,len(files))})
         files_dict = {"{}".format(i):(v, open(v, 'rb')) for i,v in enumerate(files)}
         payload = {
-            'operations': (None, '{"query": "mutation($files: [Upload!]!, $token :String!) { uploadByFile(files: $files, token:$token ) { error result taskId cannotUpload} }", "variables": { "files": ' + file_place + ' ,"token":"' + self.token  + '"}}'),
+            'operations': (None, '{"query": "mutation($files: [Upload!]!, $token :String!) { uploadByFile(files: $files, token:$token ) { error result taskIdList{file taskId} cannotUpload} }", "variables": { "files": ' + file_place + ' ,"token":"' + self.token  + '"}}'),
             'map': (None, file_map),
         }
         payload.update(files_dict)
@@ -110,7 +110,7 @@ class SensifaiApi(object):
                 data = json.loads(conn.text)['data']['uploadByFile']
                 if data['result']:
                     logger.debug("File uploaded successfully.")
-                    return data['taskId'], data['cannotUpload']
+                    return {'taskIds':data['taskIdList'], 'cannotUpload':data['cannotUpload']}
                 else:
                     logger.error("error: {}").format(data['error'])
             else:
@@ -130,10 +130,14 @@ class SensifaiApi(object):
         'query': 'query( $taskId: String! ){apiResult( taskId: $taskId){ ...on ImageResult{isDone errors imageResults{nsfwResult{type probability value}logoResult{description}landmarkResult{description}taggingResult{label probability}faceResult{detectedBoxesPercentage probability detectedFace label}}} ... on VideoResult{fps duration isDone framesCount errors videoResults{startSecond endSecond startFrame endFrame thumbnailPath taggingResult{label probability}actionResult{label probability}celebrityResult{name frequency} sportResult{label probability}nsfwResult{probability type value}}}}}',
         'variables':{'taskId':task_id}
         }
+        headers = {
+            "content-type": "application/json"
+        }
         try:
             conn = requests.post(
                                     self.host,
                                     data = json.dumps(payload),
+                                    headers = headers
                                 )
             logger.debug('HTTP Status Code: %d' % conn.status_code)
             if (conn.status_code == 200):
